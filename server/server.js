24d00ea76ec0ca42990226520eb9ab32e8b6ad4f
Dan Raskin <Danieltraskin@gmail.com>
2022-12-05 12:59:56 -0600

    server side of sse tutorial built out

--- server/server.js
+++ server/server.js
@@ -1,36 +1,97 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const SSE = require('express-sse');
+    // const SSE = require('express-sse');
+const cors = require('cors');
+const { response } = require('express');
 
 const app = express();
 
+// CORS tutorial
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/", (req, res) => res.send("hello!"));
 
-app.get("/stream", (req,res) => {
-    res.setHeader("Content-Type", "text/event-stream");
-    send(res);
-})
+//initialize server? i don''t know what this does
+app.get('/status', (req, res) => {
+    response.json({clients: clients.length})
+});
 
-let i = 0;
-function send (res) {
-    res.write("data: " + `hello!${i++}\n\n`);
-    setTimeout(()=>send(res), 1000);
+// middleware for GET requests to the /events endpoint.
+
+function eventsHandler(req, res, next) {
+    const headers = {
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
+    }
+    res.writeHead(200, headers);
+
+    const data = `data: ${JSON.stringify(facts)}\n\n`;
+
+    res.write(data);
+
+    const clientId = Date.now();
+
+    const newClient = {
+        id: clientId,
+        res
+    };
+
+    clients.push(newClient);
+
+    req.on('close', () => {
+        console.log(`${clientId} Connection closed`);
+        clients = clients.filter(client => client.id !== clientId);
+    });
 }
 
-// const sse = new SSE(["array","containing","initial","(optional)"])
+// middleware for POST requests to /fact endpoint.
 
-// app.get('/stream', sse.init);
+function sendEventsToAll(newFact) {
+    clients.forEach(client => client.res.write(
+        `data: ${JSON.stringify(newFact)}\n\n`
+    ));
+}
 
-// const content = 'hello';
-// let eventName = 'event';
-// let customID = 2;
+async function addFact(req, res, next) {
+    const newFact = req.body;
+    facts.push(newFact);
+    res.json(newFact);
+    return sendEventsToAll(newFact);
+}
 
-// sse.send(content);
-// sse.send(content, eventName);
-// sse.send(content, eventName, customID);
-// sse.updateInit(["array", "containing", "new", "content"]);
-// sse.serialize(["array", "to", "be", "sent", "as", "serialized", "events"]);
+app.post('/fact', addFact);
+
+app.get('/events', eventsHandler); 
+// youtube tutorial >>>>>>
+    // app.get("/", (req, res) => res.send("hello!"));
+
+    // app.get("/stream", (req,res) => {
+    //     res.setHeader("Content-Type", "text/event-stream");
+    //     send(res);
+    // })
+
+    // let i = 0;
+    // function send (res) {
+    //     res.write("data: " + `hello!${i++}\n\n`);
+    //     setTimeout(()=>send(res), 1000);
+    // }
+
+// express-sse stuff >>>>>>
+    // const sse = new SSE(["array","containing","initial","(optional)"])
+
+    // app.get('/stream', sse.init);
+
+    // const content = 'hello';
+    // let eventName = 'event';
+    // let customID = 2;
+
+    // sse.send(content);
+    // sse.send(content, eventName);
+    // sse.send(content, eventName, customID);
+    // sse.updateInit(["array", "containing", "new", "content"]);
+    // sse.serialize(["array", "to", "be", "sent", "as", "serialized", "events"]);
 
 
 // Serve static files
@@ -38,6 +99,9 @@ app.use(express.static('build'));
 
 // App Set //
 const PORT = process.env.PORT || 5000;
+
+let clients = [];
+let facts = [];
 
 /** Listen * */
 app.listen(PORT, () => {
