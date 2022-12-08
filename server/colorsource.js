94495e69853d5b8c6609f8b5d7c219d303e3a561
Dan Raskin <Danieltraskin@gmail.com>
2022-12-07 20:43:18 -0600

    more config. to test push

--- server/colorsource.js
+++ server/colorsource.js
@@ -28,8 +28,8 @@ const sendColor = ()=> {
     // console.log('in sendColor')
     axios({
             method: 'POST',
-            // url: 'https://git.heroku.com/sci-fair-scum/color',
-            url: 'http://localhost:5000/color',
+            url: 'https://sci-fair-scum.herokuapp.com/color',
+            // url: 'http://localhost:5000/color',
             data: newColor()
         })
     .then((res)=>{
